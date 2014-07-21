7f9048cdd5abb681b167f32113ecd29ddfbe3c4b
Jonathan Samines <jn.samines@gmail.com>
2014-07-21 11:05:30 -0600

    Creada plantilla principal del juego.

---
+++ assets/js/playsudoku.js
@@ -0,0 +1,11 @@
+/**
+ * playsudoku.js
+ * Controlador principal del juego
+ * @author Jonathan Samines
+ */
+
+function TableroSudoku(){
+	
+}
+
+TableroSudoku.prototype.dibujarTablero = function(){};
